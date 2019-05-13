d384fca54d1684998c62954d8c530f286012edc7
codedust <codedust@users.noreply.github.com>
2019-05-13 12:31:42 +0200

    Add auto scrolling, 'correct!' banner and improve partybutton size

--- static/app.js
+++ static/app.js
@@ -9,7 +9,8 @@ function nextCite(hash){
   $('.partybutton').removeClass('correct');
   $('.partybutton').removeClass('wrong');
   $('.partybutton').removeClass('desaturate');
-  $('blockquote').text('');
+  $('.correctBanner').hide();
+  $('blockquote').hide();
 
   var request_url = 'api/quote';
   try {
@@ -30,6 +31,7 @@ function nextCite(hash){
     var maxDataShortLength = 279-text.length-url.length-2;
     var quote = (data.short.length > maxDataShortLength ? data.short.substr(0, maxDataShortLength-3) + "..." : data.short);
     $('.twitterBtn').attr("href", "https://twitter.com/intent/tweet?url=" + encodeURIComponent(url) + "&text=" + encodeURIComponent(text) + encodeURIComponent("„" + quote + "“"));
+    scrollIntoViewIfNeeded(document.getElementById('quotesection'));
   });
 }
 nextCite(location.hash);
@@ -44,12 +46,31 @@ $('.partybutton').click(function(el){
     $('.partybutton[data-party="'+party+'"]').addClass('correct');
     $('.partybutton').addClass('desaturate');
     $('.partybutton[data-party="'+party+'"]').removeClass('desaturate');
+
+    $('.correctBanner span').text(current_quote.programName.replace(' zur Europawahl 2019', ''));
+    $('.correctBanner').show();
+
     $('blockquote').html('<h3>' + current_quote.headline + '</h3>' + current_quote.long + '– <a target="_blank" href="' + current_quote.url + '"><cite>' + current_quote.programName + '</cite></a>');
     $('blockquote').html($('blockquote').html().replace(current_quote.short_not_redacted, '<strong>' + current_quote.short_not_redacted + '</strong>'));
+    $('blockquote').show();
+
+    window.setTimeout(function(){
+      scrollIntoViewIfNeeded(document.getElementsByClassName('correctBannerScrollAnchor')[0]);
+    }, 50); // wait until DOM update is done
   } else {
     $('.partybutton[data-party="'+party+'"]').addClass('wrong');
   }
 });
+
+function scrollIntoViewIfNeeded(target) {
+  var rect = target.getBoundingClientRect();
+  if (rect.bottom > window.innerHeight || rect.top < 0) {
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  }
+}
 
 $('#nextCiteBtn').click(function(){
   nextCite();
